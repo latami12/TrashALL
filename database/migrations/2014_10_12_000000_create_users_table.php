fffc0a098baab26b51089de5a35e71fb2e1638f3
Latami <ppib14latami@gmail.com>
2020-12-23 10:37:13 +0700

    initial commit

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,9 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('alamat');
-            $table->string('foto');
-            $table->integer('phone');
+            $table->string('alamat')->nullable();
+            $table->string('foto')->nullable();
+            $table->integer('phone')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
