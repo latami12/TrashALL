d80702c36686cff50cb27f26aeb8a846412c1936
Latami <ppib14latami@gmail.com>
2021-01-05 13:26:23 +0700

    p

--- routes/api.php
+++ routes/api.php
@@ -23,12 +23,12 @@ Route::post('login', 'UserController@login'); // login untuk nasabah, pengurus1,
 
 Route::get('user', 'UserController@getAuthenticatedUser')->middleware('jwt.verify'); // ambil semua user
 
-Route::get('profile', 'API\ProfileController@index')->middleware('auth:api'); //ambil semua profile
-Route::patch('profile/{id}', 'API\ProfileController@update')->middleware('auth:api'); // profile update
+Route::get('profile', 'API\ProfileController@index')->middleware('jwt.verify'); //ambil semua profile
+Route::patch('profile/{id}', 'API\ProfileController@update')->middleware('jwt.verify'); // profile update
 
 
 Route::prefix('nasabah')->namespace('API')->middleware('jwt.verify')->group(function(){
-    Route::post('penjemputan', 'API\PenjemputanController@requestPenjemputan')->middleware('auth:api');
-    Route::delete('penjemputan/{id}', 'API\PenjemputanController@batalkanRequestPenjemputan')->middleware('auth:api');
+    Route::post('penjemputan', 'API\PenjemputanController@requestPenjemputan');
+    Route::delete('penjemputan/{id}', 'API\PenjemputanController@batalkanRequestPenjemputan');
     
 });
