5d4a176463deafdb40658a0196ea37104ced788c
Latami <ppib14latami@gmail.com>
2021-01-22 13:09:57 +0700

    Session Life Time

--- routes/api.php
+++ routes/api.php
@@ -31,16 +31,16 @@ Route::patch('profile/{id}', 'API\ProfileController@update')->middleware('jwt.ve
 
 
 Route::prefix('nasabah')->namespace('API')->middleware('jwt.verify')->group(function () {
-    Route::get('show-request', 'PenjemputanController@showRequestPenjemputan');
-    Route::post('request', 'PenjemputanController@requestPenjemputan');
-    Route::delete('request/cancel/{penjemputan_id}', 'PenjemputanController@batalkanRequestPenjemputan');
-    Route::delete('request/cancel-item/{detail_penjemputan_id}', 'PenjemputanController@batalkanBarangRequestPenjemputan');
+    Route::get('show-request', 'PenjemputanController@showRequestPenjemputan'); // ngambil request
+    Route::post('request', 'PenjemputanController@requestPenjemputan'); // request epnjemputan sampah nya ke pengurus satu
+    Route::delete('request/cancel/{penjemputan_id}', 'PenjemputanController@batalkanRequestPenjemputan'); // cancel penjemputan
+    Route::delete('request/cancel-item/{detail_penjemputan_id}', 'PenjemputanController@batalkanBarangRequestPenjemputan'); // cancel sampah penjemputannya
 });
 
 Route::prefix('pengurus-satu')->namespace('API')->middleware('jwt.verify')->group(function () {
-    Route::get('/show-request', 'PenyetoranController@showNasabahRequest');
-    Route::get('/show-accepted-request', 'PenyetoranController@showAcceptedRequest');
-    Route::get('/accept-request/{penjemputan_id}', 'PenyetoranController@acceptNasabahRequest');
+    Route::get('/show-request', 'PenyetoranController@showNasabahRequest'); // tampilkan request dari nasabah
+    Route::get('/show-accepted-request', 'PenyetoranController@showAcceptedRequest'); // tampilkan 
+    Route::get('/accept-request/{penjemputan_id}', 'PenyetoranController@acceptNasabahRequest'); // 
     Route::get('/decline-request/{penjemputan_id}', 'PenyetoranController@declineNasabahRequest');
     Route::get('/search-nasabah/{keyword?}', 'PenyetoranController@searchNasabah'); // Optional Parameter
     Route::post('/store', 'PenyetoranController@penyetoranNasabah');
